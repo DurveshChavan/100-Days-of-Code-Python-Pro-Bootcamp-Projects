35a148ee0069738b6bea8389d29040c40983055e
DurveshChavan <durveshchavan254@gmail.com>
2025-07-17 16:33:29 +0530

    Angela Hue: 100 Days Coding Projects

---
+++ Day 1/Band_Name_Generator.py
@@ -0,0 +1,8 @@
+# Based on the name of your city and pet program generats a Band name
+
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+
+print("Your band name could be: " + city + " " + pet )
+
